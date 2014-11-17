5636294d7bdd3dd37b8d0b5f279cd78f67fa6a26
tomsivan <tomsivanx@gmail.com>
2014-11-17 23:03:08 +0200

    fhsh

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws LexicalError {
 		Reader reader;
 		ArrayList<Token> tokArr = new ArrayList<Token>();
 		try {
