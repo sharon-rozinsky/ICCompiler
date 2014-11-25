12f39326ebf10e1d57fdb40d9aeadbcfcfd3f899
guyreuven <guyreuven88@gmail.com>
2014-11-25 19:44:01 +0200

    updating main.

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,15 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
 
+import com.sun.java_cup.internal.runtime.Symbol;
+
+import IC.AST.StatementsBlock;
 import IC.Error.LexicalError;
 
 
 public class Main {
+	
+	private static boolean printtokens = true;
 
 	public static void main(String[] args) throws LexicalError {
 		Reader reader;
@@ -18,6 +23,17 @@ public class Main {
 			{
 				reader = new FileReader(new File(args[0]));
 				Lexer lexer = new Lexer(reader);
+//				Parser parser = new Parser(scanner);
+//				parser.printTokens = printtokens;
+//				
+//				Symbol parseSymbol = parser.parse();
+//				System.out.println("Parsed " + args[0] + " successfully!");
+//				StatementsBlock root = (StatementsBlock) parseSymbol.value;
+//				
+//				// Pretty-print the program to System.out
+//				PrettyPrinter printer = new PrettyPrinter(root);
+//				printer.print();
+				
 				Token t = lexer.next_token();
 				PrintHeader();
 				while(!t.getTag().equals("EOF")){
@@ -39,6 +55,7 @@ public class Main {
 			PrintTokenError(e.getMessage());
 		}
 	}
+	
 	
 	public static void PrintToken(String token, String tag, int line, int column){
 		System.out.println(token + "\t" + tag + "\t" + line + ":" + column);
