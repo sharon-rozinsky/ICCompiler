cb1db91c4473a2d5b18646e0af44431130b0d865
Rozinsky <Rozinsky>
2014-12-05 12:17:31 +0200

    Adding support for calling two files
    Fixed the main to match what we need.
    Added support for errors in ic.cup.
    The code is ready for testing.

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
 
 import java_cup.runtime.Symbol;
 import IC.AST.ASTNode;
 import IC.AST.PrettyPrinter;
-import IC.AST.Program;
-import IC.AST.StatementsBlock;
 import IC.Error.LexicalError;
+import IC.Error.SyntaxError;
 
 
 public class Main {
@@ -19,44 +17,54 @@ public class Main {
 
 	public static void main(String[] args) throws LexicalError {
 		Reader reader;
-		ArrayList<Token> tokArr = new ArrayList<Token>();
 		try {
-			 if(args.length != 0)
-			{
+			if(args.length != 0) {
+				//parser.isDebugMode = printtokens;
+				// read the program file and parse it
 				reader = new FileReader(new File(args[0]));
 				Lexer lexer = new Lexer(reader);
-				LibraryParser parser = new LibraryParser(lexer);
-				parser.printTokens = printtokens;
-				
+				parser parser = new parser(lexer);
 				Symbol parseSymbol = parser.parse();
-				System.out.println("Parsed " + args[0] + " successfully!");
+				System.out.println("The file: " + args[0] + " was successfully parsed");
 				
-				//StatementsBlock root = (StatementsBlock) parseSymbol.value;
+				// print the AST of the program
 				PrettyPrinter printer = new PrettyPrinter(args[0]);
 				ASTNode root = (ASTNode) parseSymbol.value;
 				String output = (String) root.accept(printer);
 				System.out.println(output);
-				Token t = lexer.next_token();
-				//PrintHeader();
-				while(!Utils.getTokenName(t.getTag()).equals("EOF")){
-					PrintToken(t.getValue(), Utils.getTokenName(t.getTag()), t.getLine(), t.getColumn());
-					tokArr.add(t);
-					t = lexer.next_token();
+				reader.close();
+				
+				// Check if there is a library file and parse it
+				if(args.length == 2){
+					if(!args[1].startsWith("-L")){
+						System.out.println("The second parameter should start with -L");
+					} else{
+						String fileName = args[1].substring(2);
+						reader = new FileReader(new File(fileName));
+						Lexer libLexer = new Lexer(reader);
+						LibraryParser libParser = new LibraryParser(libLexer);
+						Symbol libParseSymbol = libParser.parse();
+						System.out.println("The file: " + args[1] + " was successfully parsed");
+						
+						printer = new PrettyPrinter(args[1]);
+						ASTNode libRoot = (ASTNode) libParseSymbol.value;
+						output = (String) libRoot.accept(printer);
+						System.out.println(output);
+						reader.close();
+					}
 				}
-			}
-			else
-			{
+			} else{
 				System.out.println("No file was given, please run again and insert which file do you want to parse!");
 			}
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e){
 			
 		} catch (LexicalError e){
 			PrintTokenError(e.getMessage());
+		} catch(SyntaxError e){
+			System.out.println(e.getMessage());
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -75,5 +83,3 @@ public class Main {
 	}
 
 }
-
-// Command to run from terminal to create: Lexer.java
