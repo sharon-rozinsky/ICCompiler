0c80b1f8dc737c6a0b3b9be25e33aba5e9c08f6a
guyreuven <guyreuven88@gmail.com>
2014-12-16 15:44:16 +0200

    add special semantic checks class

---
+++ src/IC/SemanticChecks/SpecialSemanticChecks.java
@@ -0,0 +1,12 @@
+package IC.SemanticChecks;
+
+/**
+ * this class include all the "none visitor" checks:
+ * 1. one main function check.
+ * 2. check that all local variables are initialized before used.
+ * 3. check that all none void methods returns values.
+ * 4. check correctness of library name.
+ */
+public class SpecialSemanticChecks {
+
+}
