124f3cde694be82afb0c4a5268b386d6add913b7
tomsivan <tomsivanx@gmail.com>
2014-12-27 11:56:03 +0200

    another one

--- src/lir/instructions/MoveArrayInstruction.java
+++ src/lir/instructions/MoveArrayInstruction.java
@@ -1,5 +1,39 @@
 package lir.instructions;
 
-public class MoveArrayInstruction extends Instruction {
+import lir.LirConstants;
 
+public class MoveArrayInstruction extends Instruction {
+	
+	private static final String InstructionName = "MoveArray";
+    private ArrayPair pair;
+    private Operand op;
+    private LirConstants memOp;
+	
+	public MoveArrayInstruction(ArrayPair pair, Operand op, LirConstants memOp) {
+        super(InstructionName);
+        this.pair = pair;
+        this.op = op;
+        this.memOp = memOp;
+    }
+
+    public ArrayPair getPair() {
+        return pair;
+    }
+
+    public Operand getOp() {
+        return op;
+    }
+
+    public LirConstants getMemOp() {
+        return memOp;
+    }
+    
+    @Override
+    public String toString() {
+        if (memOp.toString() == "Load") {
+            return getName() + " " + pair + "," + op; 
+        } else {
+            return getName() + " " + op + "," + pair;
+        }
+    }
 }
