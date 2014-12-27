538f73ff9ebe2e3fdf3aff3e061bc2dee51107db
tomsivan <tomsivanx@gmail.com>
2014-12-27 15:40:28 +0200

    lir printer moved to utils

--- src/IC/lir/LIRUtils.java
+++ src/IC/lir/LIRUtils.java
@@ -1,6 +1,9 @@
 package IC.lir;
 
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import IC.AST.Field;
@@ -8,6 +11,12 @@ import IC.AST.ICClass;
 import IC.AST.Method;
 import IC.AST.Program;
 import IC.AST.VirtualMethod;
+import IC.lir.instructions.Instruction;
+import IC.lir.lirObject.LIRClass;
+import IC.lir.lirObject.LIRMethod;
+import IC.lir.lirObject.LIRProgram;
+import IC.lir.lirObject.LIRStringLiteral;
+import IC.lir.operands.AddressLabel;
 
 public class LIRUtils {
 	
@@ -54,5 +63,51 @@ public class LIRUtils {
 		}
 		
 		return classLayoutMap;
+	}
+	
+	public static void printLIR(LIRProgram lirProgram, OutputStreamWriter streamWriter) {
+
+		PrintWriter printer = new PrintWriter(streamWriter);
+
+		Map<String, LIRStringLiteral> stringLiterals = lirProgram.getStringLiterals();
+		Map<String, ClassLayout> classesLayouts = lirProgram.getClassesLayout();
+		List<LIRClass> lirClasses = lirProgram.getClassList();
+
+		for (LIRStringLiteral stringLiteral : stringLiterals.values()) {
+			printer.println(stringLiteral.getLabel() + ": " + stringLiteral.getLiteral());
+		}
+
+		printer.println();
+
+		for (ClassLayout classLayout : classesLayouts.values()) {
+			printer.println(classLayout.toString());
+		}
+
+		printer.println();
+
+		for (LIRClass lirClass : lirClasses) {
+			for (LIRMethod lirMethod : lirClass.getMethods()) {
+
+				AddressLabel methodLabel = lirMethod.getMethodName();
+				AddressLabel classLabel;
+
+				if (methodLabel.getName().equals(LIRConstants.MAIN_LABEL_SUFFIX))
+					classLabel = new AddressLabel(LIRConstants.MAIN_LABEL_PREFIX);
+				else
+					classLabel = lirMethod.getContainingClassName(); 
+
+				AddressLabel label = classLabel.append(methodLabel);
+
+				printer.println(label.toString() + ":");
+
+				for (Instruction instruction : lirMethod.getInstructions()) {
+					printer.println(instruction.toString());
+				}
+
+				printer.println();
+
+			}
+		}
+
 	}
 }
